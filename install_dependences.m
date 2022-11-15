1f0b79ee2aeec364c1ebd1eba06961a1ae049ec3
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2022-11-15 13:35:00 +0100

    install_de

--- install_dependences.m
+++ install_dependences.m
@@ -5,6 +5,5 @@ unzip('https://github.com/DeustoTech/HortiMED-Modelling-Platform/archive/refs/he
 unzip('https://github.com/DeustoTech/MechanisticSolarRadiationModel/archive/refs/heads/main.zip','dependences')
 
 %%
-unzip('https://github.com/DeustoTech/KPI-Simulation-AGROSOFC/archive/refs/heads/main.zip','dependences')
 
 addpath(genpath(pwd))
