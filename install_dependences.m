69dde8c4df32e9cad060ef946002066372949bac
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2022-11-18 10:41:01 +0100

    add clima exterior

--- install_dependences.m
+++ install_dependences.m
@@ -3,6 +3,8 @@ mkdir dependences
 unzip('https://github.com/DeustoTech/HortiMED-Modelling-Platform/archive/refs/heads/main.zip','dependences')
 %%
 unzip('https://github.com/DeustoTech/MechanisticSolarRadiationModel/archive/refs/heads/main.zip','dependences')
+%%
+unzip('https://github.com/DeustoTech/HortiMED-Data-Sources/archive/refs/heads/main.zip','dependences')
 
 %%
 
